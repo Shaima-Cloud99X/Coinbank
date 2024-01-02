67b73e084cc00370f55421549e4725f45986c3e2
Shaima-Cloud99X <143798888+Shaima-Cloud99X@users.noreply.github.com>
2024-01-02 11:38:39 +0400

    Fonts

--- coinbank/src/Components/Home.js
+++ coinbank/src/Components/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
             <div className='navbarStyle'>
                 <img src = {logo} alt = 'Logo' />
             </div>
+<<<<<<< Updated upstream
             <div className="header">
             <div className="header-item">
                 <p>Features</p>
@@ -22,6 +23,14 @@ const Home = () => {
                 <p>Developing</p>
             </div>
             </div>
+=======
+
+
+            <div className='headline'>
+                <p style={{color:'#523FE8'}}>SIGN IN TO YOUR SECURE WALLET</p>
+            </div>
+            
+>>>>>>> Stashed changes
         </div>
         
     )
